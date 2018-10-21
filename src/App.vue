d7aedf87fbe718f83e6f27b19677f29787a5c770
Rafael Pontes <rafaelpontesbr@gmail.com>
2018-10-20 21:58:22 -0400

    grafico mostra os dados da api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    <h1>Dinheiro Hoje {{ this.date }}</h1>
+  <div id="app" class="flex">
     <div v-if="coins">
+      <h1>Dinheiro Hoje {{ this.date }}</h1>
       <h1>{{ coins.USD.codein }} -> {{ coins.USD.name }} / {{ coins.USD.ask }}</h1>
     </div>
     <div v-if="coinsMonth">
-      <li v-for="currency in coinsMonth">
-        {{ formatDate(currency.create_date) }} / {{ currency.code }} / {{ currency.ask }}
-      </li>
+      <highcharts :options="chartOptions"></highcharts>
+    </div>
+    <div v-if="!coins && !coinsMonth">
+      <img src="@/assets/loading.gif" alt="...Loading" class="loading">
     </div>
   </div>
 </template>
@@ -18,15 +19,52 @@ export default {
   data() {
     return {
       coin: 'USD-BRL',
-      days: '30',
+      days: '15',
       coins: '',
       coinsMonth: '',
-      date: this.formatDate()
+      date: this.formatDate(),
+      update: ''
     }
   },
-  created() {
+  mounted() {
     this.getCurrentCoins();
     this.getCoinsLastMonth();
+  },
+  computed: {
+    chartOptions() {
+      return {
+        style: {
+          fontFamily: "'Montserrat', Helvatica, Arial, sansSerif"
+        },
+        colors: [
+          '#6af9c4'
+        ],
+        title: {
+          text: 'Dólar nos ultimos 15 dias'
+        },
+        xAxis: {
+          categories: this.formatDate(
+            this.coinsMonth.map( function(value) {
+              return value.create_date;
+            })
+          )
+        },
+        yAxis: {
+          title: {
+            text: 'Valor do dólar'
+          }
+        },
+        series: [{
+          name: 'Dólar',
+          data: this.coinsMonth.map((value) => {
+            return parseFloat(value.ask);
+          })
+        }],
+        credits: {
+          enabled: false
+        }
+      }
+    }
   },
   methods: {
     async getCurrentCoins() {
@@ -43,10 +81,15 @@ export default {
     },
     formatDate(date) {
       if (date) {
+        if (Array.isArray(date)) {
+          return date.map((value) => {
+            return new Date(value).toJSON().slice(0,10).split('-').reverse().join('/');
+          });
+        }
         return new Date(date).toJSON().slice(0,10).split('-').reverse().join('/');
       }
       return new Date().toJSON().slice(0,10).split('-').reverse().join('/');
-    }
+    },
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import highchartsVue from 'highcharts-vue'
 
 axios.defaults.timeout = 10000;
 axios.defaults.baseURL = 'https://economia.awesomeapi.com.br/json/';
 
 Vue.prototype.$http = axios
+Vue.use(highchartsVue);
 
 Vue.config.productionTip = false
 
