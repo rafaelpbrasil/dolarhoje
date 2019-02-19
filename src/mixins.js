06f64cce8f84b260fa8df4a68a66bbd6325e9a0b
Rafael Pontes <rafaelpontesbr@gmail.com>
2019-02-18 20:20:39 -0400

    adicionado moment.js

--- src/mixins.js
+++ src/mixins.js
@@ -1,24 +1,28 @@
+import moment from 'moment';
+
 export default {
     methods: {
         formatDate(date) {
+            moment.locale('pt-br')
+
             if (date) {
                 if (Array.isArray(date)) {
-                return date.map((value) => {
-                    return new Date(value).toJSON().slice(0,10).split('-').reverse().join('/');
-                });
+                    return date.map((value) => {
+                        return moment(value).format('L');
+                    });
                 }
-                return new Date(date).toJSON().slice(0,10).split('-').reverse().join('/');
+                return moment(date).format('L');
             }
-            return new Date().toJSON().slice(0,10).split('-').reverse().join('/');
-            },
-            getGifPath() {
-            let gifUrl = '';
-            if (process.env.NODE_ENV === 'production') {
-                gifUrl =  'images/loading.gif';
-            } else {
-                gifUrl = './src/static/images/loading.gif'
-            }
-            return gifUrl;
-            }
+            return moment().format('L');
+        },
+        getGifPath() {
+        let gifUrl = '';
+        if (process.env.NODE_ENV === 'production') {
+            gifUrl =  'images/loading.gif';
+        } else {
+            gifUrl = './src/static/images/loading.gif'
+        }
+        return gifUrl;
+        }
     }
 }
